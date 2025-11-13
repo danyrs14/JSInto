671dc9fb82ddc105f5ec38318de91cbad1a5facc
danyrs14 <eminemrs14@gmail.com>
2025-11-13 09:59:59 -0600

    Condicionales en js

--- condicionales.js
+++ condicionales.js
@@ -1,13 +1,25 @@
-let calificaion = 5;
-//let se usa para declarar variables en js
-if (calificaion <= 5) {
-    console.log("Excelente");
-} else if (calificaion > 4) {
-    console.log("Muy Bien");
-} else if (calificaion > 3) {
-    console.log("Bien");
-} else if (calificaion > 2) {
-    console.log("Mal");
-} else if (calificaion > 1) {
-    console.log("Muy Mal");
-}
+//se importa el modulo readline
+const readline = require("readline");
+
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout
+});
+
+//cuando hacemos esto en consola, las condicionales deben ir dentro del rl.question
+rl.question("Escribe tu calificacion\n", (input) => {
+    let numero = Number(input);
+    console.log(Number(numero));
+
+    if (numero === 5) {
+        console.log("Excelente");
+    } else if (numero === 4) {
+        console.log("Muy Bien");
+    } else if (numero === 3) {
+        console.log("Bien");
+    } else if (numero === 2) {
+        console.log("Mal");
+    } else if (numero === 1) {
+        console.log("Muy Mal");
+    }
+})
